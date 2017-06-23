21f01531dbb4bd5da0843cf98bf04c6da36518d2
Tobias Schneider <schneider@synyx.de>
2017-06-23 11:11:17 +0200

    add further logging in resource configuration

--- src/main/java/coffee/synyx/config/oauth/ResourceServerConfiguration.java
+++ src/main/java/coffee/synyx/config/oauth/ResourceServerConfiguration.java
@@ -1,5 +1,7 @@
 package coffee.synyx.config.oauth;
 
+import org.slf4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,6 +14,10 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
+import static java.lang.invoke.MethodHandles.lookup;
+
 
 /**
  * @author  Tobias Schneider - schneider@synyx.de
@@ -20,6 +26,8 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableResourceServer
 @EnableConfigurationProperties(ResourceProperties.class)
 class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    private static final Logger LOGGER = getLogger(lookup().lookupClass());
 
     private final TokenStore tokenStore;
     private final ResourceProperties resourceProperties;
@@ -42,5 +50,8 @@ class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 
         resources.resourceId(resourceProperties.getId()).tokenStore(tokenStore);
+
+        LOGGER.info("//> ResourceServerSecurityConfigurer with resource id {}", resourceProperties.getId());
+        LOGGER.info("//> ResourceServerSecurityConfigurer added tokenStore");
     }
 }
